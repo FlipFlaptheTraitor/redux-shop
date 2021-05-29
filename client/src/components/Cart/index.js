13f645673dfa3d1c028c2033135c956c41256f32
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-05-29 17:11:47 -0400

    fixed provider issue

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,7 +5,6 @@ import { QUERY_CHECKOUT } from "../../utils/queries"
 import { idbPromise } from "../../utils/helpers"
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 import { useDispatch, useSelector } from 'react-redux';
@@ -17,6 +16,7 @@ const Cart = () => {
     return state
   });
 const dispatch = useDispatch();
+const [getCheckout, {data}] = useLazyQuery(QUERY_CHECKOUT);
   useEffect(() => {
     if (data) {
       stripePromise.then((res) => {
